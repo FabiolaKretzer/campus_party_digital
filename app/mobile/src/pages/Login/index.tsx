df83d65804466249926467102f77457ac3bced42
Fabiola Kretzer <fabiolakretzer@gmail.com>
2020-07-11 00:08:19 -0300

    organization components screens login and register

--- app/mobile/src/pages/Login/index.tsx
+++ app/mobile/src/pages/Login/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, KeyboardAvoidingView, TextInput, Platform } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 export default function Login() {
@@ -12,30 +12,33 @@ export default function Login() {
     navigation.navigate("Auth");
   }
   return (
-    <View>
-      <Text>Page Login</Text>
-      <TouchableOpacity
-        style={{
-          borderWidth: 2,
-          width: 60,
-          height: 30,
-          backgroundColor: "#A711b9",
-        }}
-        onPress={navigateToRegister}
-      >
-        <Text>Register</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          borderWidth: 2,
-          width: 60,
-          height: 30,
-          backgroundColor: "#E71129",
-        }}
-        onPress={navigateToHome}
-      >
-        <Text>Home</Text>
-      </TouchableOpacity>
-    </View>
+    <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding">
+      <Text>Login</Text>
+      <Text>Olá, bem-vindo!</Text>
+      <Text>Faça o login para continuar</Text>
+      <View>
+        <TextInput
+          placeholder="E-mail"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          placeholder="Senha"
+          placeholderTextColor="#999"
+          secureTextEntry={true}
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity>
+          <Text>Entrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={navigateToRegister}>
+          <Text>Não possui uma conta? Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
--- app/mobile/src/pages/Register/index.tsx
+++ app/mobile/src/pages/Register/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, KeyboardAvoidingView, TextInput, Platform } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 export default function Register() {
@@ -8,20 +8,53 @@ export default function Register() {
   function navigateBack() {
     navigation.goBack();
   }
+
+  function navigateToLogin() {
+    navigation.navigate("Login");
+  }
+
   return (
-    <View>
-      <Text>Page register</Text>
-      <TouchableOpacity
-        style={{
-          borderWidth: 2,
-          width: 60,
-          height: 30,
-          backgroundColor: "#A711b9",
-        }}
-        onPress={navigateBack}
-      >
-        <Text>Login</Text>
-      </TouchableOpacity>
-    </View>
+    <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding">
+      <Text>Cadastro</Text>
+      <Text>Olá, bem-vindo!</Text>
+      <Text>Faça seu cadastro para continuar</Text>
+      <View>
+        <TextInput
+          placeholder="Nome"
+          placeholderTextColor="#999"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          placeholder="E-mail"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          placeholder="Senha"
+          placeholderTextColor="#999"
+          secureTextEntry={true}
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+        <TextInput
+          placeholder="Confirmar senha"
+          placeholderTextColor="#999"
+          secureTextEntry={true}
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity>
+          <Text>Entrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={navigateToLogin}>
+          <Text>Não possui uma conta? Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
