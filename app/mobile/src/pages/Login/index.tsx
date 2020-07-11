1c5323336528fea8d8781d68f78f4857a70212bc
Fabiola Kretzer <fabiolakretzer@gmail.com>
2020-07-11 09:48:49 -0300

    add routes and submitt in forms login and register

--- app/mobile/src/pages/Login/index.tsx
+++ app/mobile/src/pages/Login/index.tsx
@@ -1,16 +1,34 @@
-import React from "react";
-import { Text, View, TouchableOpacity, KeyboardAvoidingView, TextInput, Platform } from "react-native";
+import React, { useState } from "react";
+import { Text, AsyncStorage, View, TouchableOpacity, KeyboardAvoidingView, TextInput, Platform, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+
+import api from '../../services/api';
 
 export default function Login() {
   const navigation = useNavigation();
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   function navigateToRegister() {
     navigation.navigate("Register");
   }
-  function navigateToHome() {
-    navigation.navigate("Auth");
+  
+  async function handleLogin() {
+    if(email != '' && password != '') {
+      const response = await api.post('/login', {
+        email,
+        password
+      });
+  
+      const { _id } = response.data;
+
+      if(_id) {
+        navigation.navigate('Home');
+      }
+    }
   }
+
   return (
     <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding">
       <Text>Login</Text>
@@ -23,16 +41,19 @@ export default function Login() {
           keyboardType="email-address"
           autoCapitalize="none"
           autoCorrect={false}
+          value={email}
+          onChangeText={setEmail}
         />
         <TextInput
           placeholder="Senha"
           placeholderTextColor="#999"
           secureTextEntry={true}
-          autoCapitalize="words"
           autoCorrect={false}
+          value={password}
+          onChangeText={setPassword}
         />
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleLogin}>
           <Text>Entrar</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={navigateToRegister}>
@@ -42,3 +63,7 @@ export default function Login() {
     </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+
+});
--- app/mobile/src/pages/Register/index.tsx
+++ app/mobile/src/pages/Register/index.tsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import { Text, View, TouchableOpacity, KeyboardAvoidingView, TextInput, Platform } from "react-native";
+import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
+
+import api from '../../services/api';
 
 export default function Register() {
   const navigation = useNavigation();
 
-  function navigateBack() {
-    navigation.goBack();
-  }
+  const [name, setName] = useState('');
+  const [companies, setCompanies] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [otherPassword, setOtherPassword] = useState('');
 
   function navigateToLogin() {
     navigation.navigate("Login");
   }
 
+  async function handleRegister() {
+    if(name != '' && companies != '' && email != '' && password != '') {
+      if(password != otherPassword) {
+        return
+      }
+
+      const response = await api.post('/register', {
+        name,
+        companies,
+        email,
+        password
+      });
+  
+      const { _id } = response.data;
+
+      if(_id) {
+        navigation.navigate('Home');
+      }
+    }
+  }
+
   return (
     <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding">
+      <TouchableOpacity onPress={navigateToLogin}>
+        <Icon 
+          name="arrow-left"
+          color="#000000"
+          size={30}
+        />
+      </TouchableOpacity>
+
       <Text>Cadastro</Text>
       <Text>Olá, bem-vindo!</Text>
       <Text>Faça seu cadastro para continuar</Text>
@@ -24,6 +58,16 @@ export default function Register() {
           placeholderTextColor="#999"
           autoCapitalize="none"
           autoCorrect={false}
+          value={name}
+          onChangeText={setName}
+        />
+        <TextInput
+          placeholder="Empresa"
+          placeholderTextColor="#999"
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={companies}
+          onChangeText={setCompanies}
         />
         <TextInput
           placeholder="E-mail"
@@ -31,28 +75,32 @@ export default function Register() {
           keyboardType="email-address"
           autoCapitalize="none"
           autoCorrect={false}
+          value={email}
+          onChangeText={setEmail}
         />
         <TextInput
           placeholder="Senha"
           placeholderTextColor="#999"
           secureTextEntry={true}
-          autoCapitalize="words"
           autoCorrect={false}
+          value={password}
+          onChangeText={setPassword}
         />
         <TextInput
           placeholder="Confirmar senha"
           placeholderTextColor="#999"
           secureTextEntry={true}
-          autoCapitalize="words"
           autoCorrect={false}
+          value={otherPassword}
+          onChangeText={setOtherPassword}
         />
 
-        <TouchableOpacity>
-          <Text>Entrar</Text>
+        <TouchableOpacity onPress={handleRegister}>
+          <Text>Cadastre-se</Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={navigateToLogin}>
-          <Text>Não possui uma conta? Cadastre-se</Text>
+          <Text>Não possui uma conta? Faça login</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
